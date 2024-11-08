4261b6123cfbec1847149bb534d590aa082c680c
iant <iant@iant.kr>
2024-11-08 14:17:17 +0900

    fix: try not use discord markdown

--- utils/send_long_text.py
+++ utils/send_long_text.py
@@ -3,7 +3,7 @@ async def send_long_text(channel, text:str):
         2000자 이상인 글 ex) 업데이트 패치 노트 를 다루기 위하여 1900자 쯤에서
         새로운 텍스트로 작성하는 코드
     '''
-
+    text = text.replace("*", "×") # Don't use discord markdown
     temp_text = ''
     for split in text.split('\n'):
         if len(temp_text) + len(split) > 1900:
